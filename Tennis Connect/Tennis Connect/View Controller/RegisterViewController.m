24c43071ad01d912a377bf09ab1d4d1ba06a279f
Angela Xu <xuangela@fb.com>
2020-07-13 12:43:46 -0700

    del user if registration not complete

--- Tennis Connect/Tennis Connect/View Controller/RegisterViewController.m
+++ Tennis Connect/Tennis Connect/View Controller/RegisterViewController.m
@@ -16,6 +16,9 @@
 @property (weak, nonatomic) IBOutlet UITextField *contactField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *dateOfBirthPicker;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *skillSegControl;
+
+@property (nonatomic, strong) UIAlertController *emptyUsernameAlert;
+
 @end
 
 @implementation RegisterViewController
@@ -25,18 +28,36 @@
     // Do any additional setup after loading the view.
 }
 
+-(void)setUpAlerts {
+    self.emptyUsernameAlert = [UIAlertController alertControllerWithTitle:@"Missing username."
+              message:@"Please input your username."
+       preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {}];
+    [self.emptyUsernameAlert addAction:okAction];
+}
+
 - (IBAction)tapRegister:(id)sender {
-    PFUser *user = [PFUser currentUser];
     
-    user[@"name"] = self.nameField.text;
-    user[@"gender"] = [NSNumber numberWithLong: self.genderSegControl.selectedSegmentIndex];
-    user[@"contact"] =self.contactField.text;
-    user[@"age"] = self.dateOfBirthPicker.date;
+    if ([self.nameField.text isEqual:@""]) {
+        [self presentViewController:self.emptyUsernameAlert animated:YES completion:^{  }];
+    } else {
+        PFUser *user = [PFUser currentUser];
+        
+        user[@"name"] = self.nameField.text;
+        user[@"gender"] = [NSNumber numberWithLong: self.genderSegControl.selectedSegmentIndex];
+        user[@"contact"] =self.contactField.text;
+        user[@"age"] = self.dateOfBirthPicker.date;
+        
+        [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) { }];
+        
+        [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+    }
     
-    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) { }];
-    
-    [self performSegueWithIdentifier:@"loginSegue" sender:nil];
-    
+}
+
+- (IBAction)tapCancel:(id)sender {
+    [[PFUser currentUser] delete];
+    [self performSegueWithIdentifier:@"delSegue" sender:nil];
 }
 
 /*
