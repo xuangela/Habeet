ced783192d73062b80c6a1e627f9b4aa94f1bc4f
Angela Xu <xuangela@fb.com>
2020-07-13 09:18:13 -0700

    project setup initial commit

---
+++ Tennis Connect/Tennis Connect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tennis Connect
+//
+//  Created by Angela Xu on 7/13/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tennis Connect/Tennis ConnectTests/Tennis_ConnectTests.m
@@ -0,0 +1,37 @@
+//
+//  Tennis_ConnectTests.m
+//  Tennis ConnectTests
+//
+//  Created by Angela Xu on 7/13/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Tennis_ConnectTests : XCTestCase
+
+@end
+
+@implementation Tennis_ConnectTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
